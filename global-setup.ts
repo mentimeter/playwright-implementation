67b8d9924682427320eb2d5cd3c758e72e295072
Oliver Stenbom <oliver@stenbom.eu>
2023-06-05 11:17:21 +0200

    Start API create user

--- global-setup.ts
+++ global-setup.ts
@@ -1,28 +1,73 @@
+import type { User } from "test";
+import { randomBytes } from "crypto";
 import { promises as fsp, existsSync } from "fs";
+import { API_URL, BASE_MENTIMETER_URL } from "test";
 
-async function createUserState(stateName: string) {
+const USER_STATE_FILE = `menti-user.json`;
+
+async function saveUserState(user: User) {
   const stateObject = {
     orgins: [
       {
-        origin: "https://wild-puma.mentimeter.app",
+        origin: BASE_MENTIMETER_URL,
         localStorage: [
           {
             name: "session_token",
-            value: "piJAkFh5aaDRpS4_ASVMZx1d1mN1NYU_MKMuc4rfJ2g.36556",
+            value: user.sessionToken,
+          },
+          // Saving these isn't required, it just eases with debugging
+          {
+            name: "email",
+            value: user.email,
+          },
+          {
+            name: "password",
+            value: user.password,
           },
         ],
       },
     ],
   };
 
-  await fsp.writeFile(stateName, JSON.stringify(stateObject));
+  await fsp.writeFile(USER_STATE_FILE, JSON.stringify(stateObject));
 }
 
-const globalSetup = async () => {
-  const stateName = `menti-user.json`;
-  if (!existsSync(stateName)) {
-    await createUserState(stateName);
-  }
-};
+const dogs = [
+  "bhotia",
+  "kerryblueterrier",
+  "icelandicsheepdog",
+  "leonberger",
+  "boerboel",
+  "tibetanmastiff",
+  "georgianshepherd",
+  "stephensstock",
+  "segugioitaliano",
+  "bergamascoshepherd",
+  "miniaturebullterrier",
+];
 
-export default globalSetup;
+async function createUser(): Promise<User> {
+  const dogName = dogs[Math.floor(Math.random() * dogs.length)];
+  const nowTime = Math.floor(new Date().getTime() / 1000);
+  const randomChars = randomBytes(6).toString("hex");
+
+  const email = `${dogName}-${nowTime}-${randomChars}@testemail.com`;
+  const password = "12345678";
+
+  // TODO change to post with {email, password} body
+  const post_resp = await fetch(`${API_URL}/users`);
+  const user = await post_resp.json();
+
+  return {
+    email,
+    password,
+    sessionToken: user.session_token,
+  };
+}
+
+export default async function () {
+  if (!existsSync(USER_STATE_FILE)) {
+    const user = await createUser();
+    await saveUserState(user);
+  }
+}
--- test.ts
+++ test.ts
@@ -1,11 +1,21 @@
 import { test as baseTest } from "@playwright/test";
 
+export const BASE_MENTIMETER_URL = "https://wild-puma.mentimeter.app";
+export const VOTING_URL = "https://wild-puma.menti.app";
+export const API_URL = "https://wild-puma.api.mentimeter.app";
+
+export interface User {
+  email: string;
+  password: string;
+  sessionToken: string;
+}
+
 export const test = baseTest.extend<{
   mentimeterURL: string;
   votingURL: string;
   apiURL: string;
 }>({
-  mentimeterURL: "https://wild-puma.mentimeter.app",
-  votingURL: "https://wild-puma.menti.app",
-  apiURL: "https://wild-puma.api.mentimeter.app",
+  mentimeterURL: BASE_MENTIMETER_URL,
+  votingURL: VOTING_URL,
+  apiURL: API_URL,
 });

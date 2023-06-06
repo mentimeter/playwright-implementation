4491de4caf29e32859647db45389cf103d42823f
Charlotte Andersson <charlotteandersson6@gmail.com>
2023-06-06 14:03:49 +0200

    Checkpoint 1

--- tests/user-signup.spec.ts
+++ tests/user-signup.spec.ts
@@ -1,7 +1,13 @@
+import { expect } from "@playwright/test";
 import { test } from "test";
 import { SignUpPage } from "pages";
 
 test("user can sign up", async ({ page, mentimeterURL }) => {
   const signUpPage = new SignUpPage(page, mentimeterURL);
   await signUpPage.goto();
+  await signUpPage.fillSignupForm();
+
+  await signUpPage.signUpButton.click();
+  // The first thing a user sees on successful signup is a an onboarding modal
+  await expect(signUpPage.skipButton).toBeVisible();
 });

79c86b01cd01c7b7f588b0268af3e97c739be0b1
Charlotte Andersson <charlotteandersson6@gmail.com>
2023-06-06 13:43:22 +0200

    downgrade to 5

--- global-teardown.ts
+++ global-teardown.ts
@@ -1,12 +1,1 @@
-import { deleteUser } from "lib";
-import { promises as fsp } from "fs";
-import { USER_STATE_FILE } from "test";
-
-export default async function () {
-  try {
-    await deleteUser();
-    await fsp.rm(USER_STATE_FILE);
-  } catch (e) {
-    throw new Error(`Error while deleting user in cleanup: ${e.message}`);
-  }
-}
+export default async function () {}

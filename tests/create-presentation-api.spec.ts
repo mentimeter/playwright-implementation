a730a59dee178cf94a4482d2db064242e359ab3b
Oliver Stenbom <oliver@stenbom.eu>
2023-06-06 12:05:00 +0300

    Rename create-presentation-api, update expects

--- tests/create-presentation-api.spec.ts
+++ tests/create-presentation-api.spec.ts
@@ -1,6 +1,6 @@
 import { test } from "test";
 import { expect } from "@playwright/test";
-import { HomeviewPage } from "pages";
+import { EditorPage, HomeviewPage } from "pages";
 import { USER_STATE_FILE } from "test";
 
 test.use({ storageState: USER_STATE_FILE });
@@ -11,8 +11,11 @@ test("user can create a presentation via API", async ({
   createPresentation,
 }) => {
   const presentation = await createPresentation();
-  const homeview = new HomeviewPage(page, mentimeterURL);
-
-  await homeview.goto();
   expect(presentation.name).toEqual("Lottas prez");
+
+  const editorPage = new EditorPage(page, mentimeterURL);
+  editorPage.gotoPresentation(presentation);
+
+  const title = await editorPage.presentationName.inputValue();
+  expect(title).toBe("Lottas prez");
 });
